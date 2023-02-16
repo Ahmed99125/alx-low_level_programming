10fcdb7f37738add31ebb7198b814847a61b1501
Ahmed99125 <at99125@gmail.com>
2023-02-16 10:12:06 -0800

    cmt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include "stdio.h"
+
+/**  * main - Entry point  *  * Description: 'the program's description'  *  * Return: Always 0 (Success)  */
+
 int main(void)
 {
-/*besmellah*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
