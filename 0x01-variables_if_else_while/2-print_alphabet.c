acf8e28bdaeeef0ae79c4277f21a86ae46fe398d
Ahmed99125 <at99125@gmail.com>
2023-02-17 23:03:27 -0800

    cmt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 26, c = 97;
+
+	while (n--)
+	{
+		putchar(c++);
+		printf("\n");
+	}
+	return (0);
+}
