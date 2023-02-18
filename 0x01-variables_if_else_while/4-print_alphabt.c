d9e86b0f34c5fe8b7ab558c7cd7b687047edc4bb
Ahmed99125 <at99125@gmail.com>
2023-02-17 23:11:13 -0800

    cmt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 26, c = 97;
+
+	while (n--)
+	{
+		if (c == 101 || c == 113)
+			continue;
+		putchar(c++);
+	}
+	printf("\n");
+	return (0);
+}
