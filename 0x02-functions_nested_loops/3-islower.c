97292e78b18190308421b9ba184b61b2005eca48
Ahmed99125 <at99125@gmail.com>
2023-02-22 17:02:18 -0800

    cmt

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 /**
  * _islower - entry point
- * Return: always 0
+ * @c : the char to be checked
+ * Return: 1 for lowercase or 0 for anything else
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
