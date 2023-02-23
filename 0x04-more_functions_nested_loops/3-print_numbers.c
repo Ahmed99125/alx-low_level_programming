9008994f67d71a8e072d87f8c8cf1b51f1d1999a
Ahmed99125 <at99125@gmail.com>
2023-02-23 05:15:33 -0800

    cmt

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return;
+}
