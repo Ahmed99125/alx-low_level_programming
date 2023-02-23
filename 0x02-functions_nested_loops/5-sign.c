00511b551f976a46d124348aee33d0b9a25d09a2
Ahmed99125 <at99125@gmail.com>
2023-02-22 17:38:52 -0800

    cmt

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * print_sign - entry point
- * @n - the parameter to be checked
+ * @n: the parameter to be checked
  * Return: 1 for positive and -1 for negative
  */
 int print_sign(int n)
 {
-	if (n > 0) 
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
